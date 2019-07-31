af1711d26608aff2077b2c1d03e3a315c05fb00b
Louisa Evola <le9ds@virginia.edu>
2019-07-31 17:46:14 -0400

    fixed bug

--- src/components/Bills/Picture.js
+++ src/components/Bills/Picture.js
@@ -5,13 +5,8 @@ export default function Picture() {
 
 return (
   <div className={styles.billImage}>
-<<<<<<< HEAD
     <img src='https://www.micronic.com/uploads/news/a-more-sustainable-choice-for-sample-storage-1.png' width= "150px" height="150px"/>
-    <img src='https://www.micronic.com/uploads/news/a-more-sustainable-choice-for-sample-storage-1.png' class="boom" width= "150px" height="150px"/>
-    <img src='https://www.micronic.com/uploads/news/a-more-sustainable-choice-for-sample-storage-1.png' width= "150px" height="150px"/>
-=======
-    <img src='https://www.micronic.com/uploads/news/a-more-sustainable-choice-for-sample-storage-1.png' class="boom" width= "150px" height="150px"/>
->>>>>>> fixed merge conflicts
+    <img src='https://www.micronic.com/uploads/news/a-more-sustainable-choice-for-sample-storage-1.png' className="boom" width= "150px" height="150px"/>
   </div>
   )
 }
